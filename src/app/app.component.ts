6d639fc9e08f14801304448633ea1a052bc9163e
lucky <mamathauppal@gmail.com>
2021-02-06 20:29:54 +0530

    employee details

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'EmployeeApp';
+   min:any;
+  employees: any[] = [];
+
+  constructor() {
+    this.employees = [
+      { eid: 1, ename: 'a', sal: 100 },
+      { eid: 2, ename: 'b', sal: 1000 },
+      { eid: 3, ename: 'c', sal: 50 },
+      { eid: 4, ename: 'd', sal: 500 },
+      { eid: 5, ename: 'e', sal: 300 }
+    ]
+    this.min = this.employees.reduce(function(old, item) {
+      return (old.sal < item.sal) ? old : item
+      }) 
+   console.log(this.min);
+  }  
+  
+}
